5625ee00da5c36a3be6b7f9117156680beae4687
berger <robertberger.dev@gmail.com>
2023-02-09 14:02:47 -0500

    added contacts from lesson 2 example into Contacts.js

---
+++ src/Contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+    {
+      first_name: "Ross",
+      last_name: "Jacobson",
+      email: "ross.jacboson@example.com",
+      phone: "687-676-5513"
+    },
+    {
+      first_name: "Serena",
+      last_name: "Durgan",
+      email: "serena.durgan@example.net",
+      phone: "696-357-2316"
+    },
+    {
+      first_name: "Christoper",
+      last_name: "Upton",
+      email: "christopher.upton@example.org",
+      phone: "443-076-9449"
+    },
+    {
+      first_name: "Lance",
+      last_name: "Schiller",
+      email: "lance.schller@example.com",
+      phone: "581-410-9513"
+    }
+  ]
+  
+  export default contacts
