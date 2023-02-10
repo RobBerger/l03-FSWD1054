3697cc13ff13a87c90906cbae2d18908c0a94a47
berger <robertberger.dev@gmail.com>
2023-02-10 17:53:02 -0500

    can't get it to work

--- src/App.js
+++ src/App.js
@@ -2,31 +2,11 @@ import React from 'react';
 import contacts from './Contacts.js'
 import Contact from './Contact.js'
 
-let contactList = contacts.map((contact, index) => {
-  return (<Contact contact={contact} />)
-})
-
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contactList}
-        </tbody>
-      </table>
-      <div>
-        {contacts.length ?
-          <p>Total Number of Contacts: <span>{contacts.length}</span></p> :
-          <p>Sorry, no contacts found :(</p> }
-      </div>
-    </div>
-  )
+function App() {
+  let contactList = contacts.map((contact, index) => {
+    return (<Contact contact={contact} />)
+  })
 }
+
 
 export default App;
--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,11 @@
-function Contact() {
-    return (<div>This is a contact component</div>)
+function Contact(props) {
+    return (
+      <tr>
+        <td>{props.contact.first_name} {props.contact.last_name}</td>
+        <td><a href={`mailto:${props.contact.email}`}>{props.contact.email}</a></td>
+        <td>{props.contact.phone}</td>
+      </tr>
+    )
   }
   
   export default Contact
