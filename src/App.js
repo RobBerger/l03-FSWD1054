bc9e9d5cab9aca9ac2d48f9de87d08557f353aa6
berger <robertberger.dev@gmail.com>
2023-02-10 17:41:37 -0500

    updated contactList return in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import contacts from './Contacts.js'
 import Contact from './Contact.js'
 
-function App() {
-  let contactList = contacts.map((contact, index) => {
-    return (<Contact />)
-  })
+let contactList = contacts.map((contact, index) => {
+  return (<Contact contact={contact} />)
+})
 
   return (
     <div>
