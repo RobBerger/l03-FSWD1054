700ce2bdc93a4e601c359e95a6d9ecd664b49a9e
berger <robertberger.dev@gmail.com>
2023-02-13 12:22:52 -0500

    removed old css stuff

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import styles from './Contact.module.css'
 
 class Contact extends React.Component {
   render() {
   let { contact: { first_name, last_name, email, phone } } = this.props
   return (
-    <tr className={styles.contact}>
-      <td className={styles.name}>{first_name} {last_name}</td>
+    <tr>
+      <td>{first_name} {last_name}</td>
       <td><a href={`mailto:${email}`}>{email}</a></td>
       <td>{phone}</td>
     </tr>
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Contact from './Contact.js'
-import styles from './ContactList.module.css'
 import Table from 'react-bootstrap/Table'
 
 class ContactList extends React.Component {
 
   headings() {
     let headings = ["Name", "Email", "Phone Number"]
-    return <tr className={styles.heading}>{headings.map((heading) => <th>{heading}</th>)}</tr>
+    return <tr>{headings.map((heading) => <th>{heading}</th>)}</tr>
   }
 
   contacts() {
