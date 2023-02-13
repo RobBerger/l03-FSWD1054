f08db755bee314e3d8e93012206137dd3fa346ef
berger <robertberger.dev@gmail.com>
2023-02-13 09:09:41 -0500

    going through the lesson again

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import Contact from './Contact.js'
 
 function App() {
   let contactList = contacts.map((contact, index) => {
-    return (<Contact contact={contact} />)
+    return (<Contact contact={contact}/>)
   })
-}
 
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList}
+        </tbody>
+      </table>
+      <div>
+        {contacts.length ?
+          <p>Total Number of Contacts: <span>{contacts.length}</span></p> :
+          <p>Sorry, no contacts found :(</p> }
+      </div>
+    </div>
+  )
+}
 
 export default App;
--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,5 @@
 function Contact(props) {
-    return (
-      <tr>
-        <td>{props.contact.first_name} {props.contact.last_name}</td>
-        <td><a href={`mailto:${props.contact.email}`}>{props.contact.email}</a></td>
-        <td>{props.contact.phone}</td>
-      </tr>
-    )
+  return (<div>This is a contact componet</div>)
   }
   
   export default Contact
