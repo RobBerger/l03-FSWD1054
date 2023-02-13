ac2da2b99125a2082815fef3ef12bd5c51ba443d
berger <robertberger.dev@gmail.com>
2023-02-13 11:43:17 -0500

    connected ContactSummary css

--- src/ContactSummary.js
+++ src/ContactSummary.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './ContactSummary.module.css'
 
 class ContactSummary extends React.Component {
   render() {
     return (
-    <div>
+    <div className={styles.summary}>
      { this.props.contacts.length ?
        <p>Total Number of Contacts: <span>{this.props.contacts.length}</span></p> :
        <p>Sorry, no contacts found :(</p> }
