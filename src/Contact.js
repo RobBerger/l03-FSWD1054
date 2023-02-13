ff8d750c74a1df009469cb86a5fe769df48fff30
berger <robertberger.dev@gmail.com>
2023-02-13 09:29:50 -0500

    got table to work

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,11 @@
 function Contact(props) {
-  return (<div>This is a contact componet</div>)
-  }
+  return (
+    <tr>
+      <td>{props.contact.first_name} {props.contact.last_name}</td>
+      <td><a href={`mailto:${props.contact.email}`}>{props.contact.email}</a></td>
+      <td>{props.contact.phone}</td>
+    </tr>
+  )
+}
   
   export default Contact
