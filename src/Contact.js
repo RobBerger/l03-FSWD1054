53f282789687ad421ae405756ad869aa6575bd1d
berger <robertberger.dev@gmail.com>
2023-02-13 11:35:13 -0500

    connected the contact css

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
+import styles from './Contact.module.css'
 
 class Contact extends React.Component {
   render() {
   let { contact: { first_name, last_name, email, phone } } = this.props
   return (
-    <tr>
-      <td>{first_name} {last_name}</td>
+    <tr className={styles.contact}>
+      <td className={styles.name}>{first_name} {last_name}</td>
       <td><a href={`mailto:${email}`}>{email}</a></td>
       <td>{phone}</td>
     </tr>
